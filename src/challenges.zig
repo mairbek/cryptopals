a23467c50b4811c4cd7afee06a9c5ceee578e3f2
Mairbek Khadikov <mairbek@conduit.xyz>
2024-06-18 21:32:15 -0700

    challenge 2

--- src/challenges.zig
+++ src/challenges.zig
@@ -36,3 +36,37 @@ pub fn convertHexToBase64(allocator: *std.mem.Allocator, hex: []const u8) ![]con
     const encoded = std.base64.standard.Encoder.encode(buffer, b);
     return encoded;
 }
+
+fn byteToHex(byte: u8) []const u8 {
+    const hex_chars = "0123456789abcdef";
+    return &[_]u8{ hex_chars[byte >> 4], hex_chars[byte & 0x0F] };
+}
+
+fn bytesToHex(allocator: *std.mem.Allocator, bytes: []const u8) ![]u8 {
+    var hexString = try allocator.alloc(u8, bytes.len * 2);
+
+    for (0.., bytes) |idx, byte| {
+        const hex = byteToHex(byte);
+        hexString[idx * 2] = hex[0];
+        hexString[idx * 2 + 1] = hex[1];
+    }
+
+    return hexString;
+}
+
+pub fn fixedXor(allocator: *std.mem.Allocator, a: []const u8, b: []const u8) ![]const u8 {
+    if (a.len != b.len) {
+        return error.InvalidXorLength;
+    }
+    const ba = try hexToBytes(allocator, a);
+    defer allocator.free(ba);
+    const bb = try hexToBytes(allocator, b);
+    defer allocator.free(bb);
+    const result = try allocator.alloc(u8, ba.len);
+    defer allocator.free(result);
+    for (0.., ba, bb) |i, ai, bi| {
+        result[i] = ai ^ bi;
+    }
+    const hex = try bytesToHex(allocator, result);
+    return hex;
+}
--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,9 @@ pub fn main() !void {
 
     const challenge1 = try challeges.convertHexToBase64(&allocator, "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
     defer allocator.free(challenge1);
-
     std.debug.print("Challenge 1 {s} \n", .{challenge1});
+
+    const challenge2 = try challeges.fixedXor(&allocator, "1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965");
+    defer allocator.free(challenge2);
+    std.debug.print("Challenge 2 {s} \n", .{challenge2});
 }
