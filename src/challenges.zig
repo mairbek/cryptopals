0cf719af97bef337ae861c739604645cc24d3b5d
Mairbek Khadikov <mairbek@conduit.xyz>
2024-08-15 20:40:28 -0700

    challenge 3

--- src/challenges.zig
+++ src/challenges.zig
@@ -12,7 +12,7 @@ fn hexDigitToInt(digit: u8) !u8 {
     }
 }
 
-fn hexToBytes(allocator: *std.mem.Allocator, hex: []const u8) ![]const u8 {
+pub fn hexToBytes(allocator: *std.mem.Allocator, hex: []const u8) ![]const u8 {
     const len = hex.len;
     if (len % 2 != 0) {
         return error.InvalidHexLength;
@@ -25,6 +25,15 @@ fn hexToBytes(allocator: *std.mem.Allocator, hex: []const u8) ![]const u8 {
         rawBytes[i / 2] = (highNibble << 4) | lowNibble;
     }
     return rawBytes;
+}
+
+pub fn xor(allocator: *std.mem.Allocator, bytes: []const u8, ch: u8) ![]const u8 {
+    var result = try allocator.alloc(u8, bytes.len);
+    var i: usize = 0;
+    while (i < bytes.len) : (i += 1) {
+        result[i] = bytes[i] ^ ch;
+    }
+    return result;
 }
 
 // TODO(mairbek): implement base64 encoding in Zig.
@@ -69,4 +78,59 @@ pub fn fixedXor(allocator: *std.mem.Allocator, a: []const u8, b: []const u8) ![]
     }
     const hex = try bytesToHex(allocator, result);
     return hex;
+}
+
+pub fn computeDistribution(bytes: []const u8) f32 {
+    var result: [27]u8 = [_]u8{0} ** 27;
+    var sum: u8 = 0;
+    for (bytes) |byte| {
+        if (byte >= 'a' and byte <= 'z') {
+            result[byte - 'a'] += 1;
+        } else if (byte >= 'A' and byte <= 'Z') {
+            result[byte - 'A'] += 1;
+        } else {
+            result[26] += 1;
+        }
+        sum += 1;
+    }
+    const letterDistribution: [27]f32 = [_]f32{
+        8.167, // a
+        1.492, // b
+        2.782, // c
+        4.253, // d
+        12.702, // e
+        2.228, // f
+        2.015, // g
+        6.094, // h
+        6.966, // i
+        0.153, // j
+        0.772, // k
+        4.025, // l
+        2.406, // m
+        6.749, // n
+        7.507, // o
+        1.929, // p
+        0.095, // q
+        5.987, // r
+        6.327, // s
+        9.056, // t
+        2.758, // u
+        0.978, // v
+        2.360, // w
+        0.150, // x
+        1.974, // y
+        0.074, // z
+        0.0, // other
+    };
+
+    var norm: [27]f32 = [_]f32{0.0} ** 27;
+    var score: f32 = 0.0;
+    for (0..27) |i| {
+        norm[i] = @as(f32, @floatFromInt(result[i])) / @as(f32, @floatFromInt(sum));
+        // std.debug.print("Norm {d:.3} / {d:.3} = {d:.3} \n", .{ @as(f32, @floatFromInt(result[i])), @as(f32, @floatFromInt(sum)), norm[i] });
+        const err: f32 = norm[i] - (letterDistribution[i] / 100.0);
+        score += (err * err);
+        // std.debug.print("{any} Score: {d:.3} err {d:.3}' \n", .{ i, score, err });
+    }
+    return score;
 }
--- src/main.zig
+++ src/main.zig
@@ -11,4 +11,23 @@ pub fn main() !void {
     const challenge2 = try challeges.fixedXor(&allocator, "1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965");
     defer allocator.free(challenge2);
     std.debug.print("Challenge 2 {s} \n", .{challenge2});
+
+    var i: u8 = 0;
+    const challenge3bytes = try challeges.hexToBytes(&allocator, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+    var minScore: f32 = 100.0;
+    var minChar: u8 = 'A';
+    while (i < 26) {
+        const ch: u8 = 'A' + i;
+
+        const candidate = try challeges.xor(&allocator, challenge3bytes, ch);
+        defer allocator.free(candidate);
+
+        const score: f32 = challeges.computeDistribution(candidate);
+        if (score < minScore) {
+            minScore = score;
+            minChar = ch;
+        }
+        i += 1;
+    }
+    std.debug.print("Challenge 3 {c} \n", .{minChar});
 }
