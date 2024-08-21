3fb5169a3e069d9ad76b1d487e2a026f9994bf6c
Mairbek Khadikov <mairbek@conduit.xyz>
2024-08-20 20:38:29 -0700

    moving code

--- src/byteutil.zig
+++ src/byteutil.zig
@@ -53,13 +53,11 @@ fn byteToHex(byte: u8) []const u8 {
 
 fn bytesToHex(allocator: *std.mem.Allocator, bytes: []const u8) ![]u8 {
     var hexString = try allocator.alloc(u8, bytes.len * 2);
-
     for (0.., bytes) |idx, byte| {
         const hex = byteToHex(byte);
         hexString[idx * 2] = hex[0];
         hexString[idx * 2 + 1] = hex[1];
     }
-
     return hexString;
 }
 
@@ -78,63 +76,4 @@ pub fn fixedXor(allocator: *std.mem.Allocator, a: []const u8, b: []const u8) ![]
     }
     const hex = try bytesToHex(allocator, result);
     return hex;
-}
-
-pub fn calculateScore(bytes: []const u8) f32 {
-    const letterDistribution: [26]f32 = [_]f32{
-        8.167, // a
-        1.492, // b
-        2.782, // c
-        4.253, // d
-        12.702, // e
-        2.228, // f
-        2.015, // g
-        6.094, // h
-        6.966, // i
-        0.153, // j
-        0.772, // k
-        4.025, // l
-        2.406, // m
-        6.749, // n
-        7.507, // o
-        1.929, // p
-        0.095, // q
-        5.987, // r
-        6.327, // s
-        9.056, // t
-        2.758, // u
-        0.978, // v
-        2.360, // w
-        0.150, // x
-        1.974, // y
-        0.074, // z
-    };
-    var result: f32 = 0;
-    for (bytes) |b| {
-        if (b >= 'a' and b <= 'z') {
-            result += letterDistribution[b - 'a'];
-        } else if (b >= 'A' and b <= 'Z') {
-            result += letterDistribution[b - 'A'];
-        } else if (b == ' ') {
-            result += 20.0;
-        }
-    }
-    return result;
-}
-
-pub fn maxScore(bytes: []const u8, from: u8, to: u8) !struct { u8, f32 } {
-    var allocator = std.heap.page_allocator;
-
-    var maxS: f32 = 0;
-    var maxI: u8 = 0;
-    var i: u8 = from;
-    while (i <= to) : (i += 1) {
-        const candidate = try xor(&allocator, bytes, i);
-        const score = calculateScore(candidate);
-        if (score > maxS) {
-            maxS = score;
-            maxI = i;
-        }
-    }
-    return .{ maxI, maxS };
 }
---
+++ src/english.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+const byteutil = @import("byteutil.zig");
+
+pub fn scoreText(bytes: []const u8) f32 {
+    const letterDistribution: [26]f32 = [_]f32{
+        8.167, // a
+        1.492, // b
+        2.782, // c
+        4.253, // d
+        12.702, // e
+        2.228, // f
+        2.015, // g
+        6.094, // h
+        6.966, // i
+        0.153, // j
+        0.772, // k
+        4.025, // l
+        2.406, // m
+        6.749, // n
+        7.507, // o
+        1.929, // p
+        0.095, // q
+        5.987, // r
+        6.327, // s
+        9.056, // t
+        2.758, // u
+        0.978, // v
+        2.360, // w
+        0.150, // x
+        1.974, // y
+        0.074, // z
+    };
+    var result: f32 = 0;
+    for (bytes) |b| {
+        if (b >= 'a' and b <= 'z') {
+            result += letterDistribution[b - 'a'];
+        } else if (b >= 'A' and b <= 'Z') {
+            result += letterDistribution[b - 'A'];
+        } else if (b == ' ') {
+            result += 20.0;
+        }
+    }
+    return result;
+}
+
+pub fn maxScore(bytes: []const u8, from: u8, to: u8) !struct { u8, f32 } {
+    var allocator = std.heap.page_allocator;
+
+    var maxS: f32 = 0;
+    var maxI: u8 = 0;
+    var i: u8 = from;
+    while (i <= to) : (i += 1) {
+        const candidate = try byteutil.xor(&allocator, bytes, i);
+        const score = scoreText(candidate);
+        if (score > maxS) {
+            maxS = score;
+            maxI = i;
+        }
+    }
+    return .{ maxI, maxS };
+}
